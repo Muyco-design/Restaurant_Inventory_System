d45960e95b2720f87d51a12647a642e0ff7d29be
Muyco-design <paulbusgano@gmail.com>
2025-12-03 08:42:15 +0800

    Updates and New Order Page

---
+++ src/main/java/com/example/OrderPage.java
@@ -0,0 +1,83 @@
+package com.example;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+
+public class OrderPage {
+
+  
+    @FXML
+    private Button DashboardButton;
+
+    @FXML
+    private ImageView Dashboardicon;
+
+    @FXML
+    private AnchorPane Inventory;
+
+    @FXML
+    private Button InventoryButton;
+
+    @FXML
+    private Button InventoryButton1;
+
+    @FXML
+    private Button MenuButton;
+
+    @FXML
+    private ImageView ProfilePicture;
+
+    @FXML
+    private TextField SearchBarMenu;
+
+    @FXML
+    private Button confirmOrderButton;
+
+    @FXML
+    private Button goBack;
+
+    @FXML
+    private HBox logOffBox;
+
+    @FXML
+    private VBox orderContainer;
+
+    @FXML
+    private AnchorPane orderPanel;
+
+    @FXML
+    private ScrollPane orderScrollPane;
+
+    @FXML
+    private Label orderTitle;
+
+    @FXML
+    private HBox totalCont;
+
+    @FXML
+    private Label totalPriceLabel;
+
+    @FXML
+    void goBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleDashboard(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleMenuButton(ActionEvent event) {
+
+    }
+  
+}
